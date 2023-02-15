9b24ba7f812bbe184b753e2f7cc729ce1185f384
Neil Bizzell <NBizzell@users.noreply.github.com>
2023-02-15 13:01:07 +0000

    Revert "removed users input to prevent compute issues"
    This reverts commit adacb95f1b78071dbe21b16b813a9c4e971db6bb.

--- Hello world.py
+++ Hello world.py
@@ -31,8 +31,51 @@ print(total)
 
 # COMMAND ----------
 
+# DBTITLE 1,User Input
+number1 = input("Enter a number")
+number2 = input("Enter a second number")
+
+total = number1 + number2
+print(total)
+
+# COMMAND ----------
+
+# DBTITLE 1,Casting
+number1 = input("Enter a number")
+number2 = input("Enter a second number")
+
+total = int(number1) + int(number2)
+print(total)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Alternately if the inputted value will alway be treated As a number then cast at the point of input
+
+# COMMAND ----------
+
+# DBTITLE 1,Casting at point of input
+number1 = int(input("Enter a number"))
+number2 = int(input("Enter a second number"))
+
+total = number1 + number2
+print(total)
+
+# COMMAND ----------
+
 # DBTITLE 1,Mathematical Operators
 print(7/5)
 print(7//5)
 print(7%5)
 print(7//5, "r", 7%5)
+
+# COMMAND ----------
+
+# DBTITLE 1,Average
+number1 = input("Enter a number")
+number2 = input("Enter a second number")
+number3 = input("Enter a third number")
+
+average = (int(number1) + int(number2) + int(number3)) / 3
+           
+print("average:", average)
